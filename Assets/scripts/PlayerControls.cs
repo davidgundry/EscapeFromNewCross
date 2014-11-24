9dae191665916070a12669ac6ec9598870c55c6d
davidgundry <davidgundry@gmail.com>
2014-11-24 11:27:28 +0000

    Basic character controller

--- Assets/scripts/PlayerControls.cs
+++ Assets/scripts/PlayerControls.cs
@@ -5,10 +5,15 @@ public class PlayerControls : MonoBehaviour {
 
 
 	public float speed;
+	public float jumpSpeed;
+	public float gravity;
+	public float yVelocity;
+
+	private CharacterController cController;
 	
 	// Use this for initialization
 	void Start () {
-	
+	  cController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +23,22 @@ public class PlayerControls : MonoBehaviour {
 	
 	void FixedUpdate ()
 	{
-	
+	  
+	  if (cController.isGrounded)
+	  {
+	      yVelocity = 0;
+	      if (Input.GetButtonDown("Jump"))
+		  yVelocity = jumpSpeed;
+	  }
+	  else
+	  {
+	      yVelocity -= gravity;
+	  }
 	  float moveHorizontal = Input.GetAxis("Horizontal");
 	  float moveVertical = Input.GetAxis("Vertical");
 	  Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
-	  rigidbody.AddForce(movement * speed * Time.deltaTime);
- 
+	  cController.Move(movement * speed *  Time.deltaTime);
+	  cController.Move(new Vector3(0.0f,yVelocity,0.0f) * Time.deltaTime);
 	}
 	
 	
