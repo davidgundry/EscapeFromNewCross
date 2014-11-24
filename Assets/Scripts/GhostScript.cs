dd39838d18b258d4f9ea6751d4ec1a16137bf0a6
Joe Cutting <Joe Cutting>
2014-11-24 15:15:02 +0000

    Ghost doesn't hit walls

--- Assets/Scripts/GhostScript.cs
+++ Assets/Scripts/GhostScript.cs
@@ -11,30 +11,36 @@ enum Direction {
 public class GhostScript : MonoBehaviour {
 
 	private Direction direction;
-	private Vector2[] directionVectors = new Vector2[] { new Vector2(0,-1),new Vector2(0,1),new Vector2(-1,0),new Vector2(1,0)};
+	private Vector2[] directionVectors = new Vector2[] { new Vector2(0,1),new Vector2(0,-1),new Vector2(-1,0),new Vector2(1,0)};
 	private Vector3 movement;
 	public float speed;
+	private bool moveBack;
+	//private Vector2 backVector;
 
 
 	// Use this for initialization
 	void Start () {
 		setDirection(Direction.down);
+		moveBack = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	void OnCollisionEnter (Collision col)
+
+	void OnCollisionEnter(Collision col)
 	{
 		if(col.gameObject.tag == "wall")
 		{
+			moveBack = true;
 			Debug.Log ("Hit wall "+direction);
-			rigidbody.AddForce(-movement * speed * Time.deltaTime*4.0f);
-			setDirection(direction-1);
 
-			Debug.Log ("Hit wall "+direction);
+
 		}
+	}
+	void OnCollisionExit(Collision collision)
+	{
+		Debug.Log ("Not Hitting wall "+direction);
+		moveBack = false;
+		//setDirection(direction-1);
+		setDirection((Direction)Random.Range ((int)Direction.up,(int)Direction.right));
 	}
 	void setDirection(Direction newDirection) {
 		direction = newDirection;
@@ -47,19 +53,18 @@ public class GhostScript : MonoBehaviour {
 			rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
 		}
 	}
-	void OnCollideEnter(Collider other) {
-	
-		/*if (other.gameObject.tag == "wall") {
 
-
-		}*/
-	}
 	void FixedUpdate () {
 
 		Vector2 moveVector = directionVectors[(int)direction];
 		movement = new Vector3(moveVector.x, 0.0f, moveVector.y);
 		//Vector3 movement = new Vector3(-1f, 0.0f, 0f);
 		//rigidbody.Move(movement* speed * Time.deltaTime);
-		rigidbody.AddForce(movement * speed * Time.deltaTime);
+		if (moveBack) {
+				rigidbody.AddForce (movement * speed * Time.deltaTime);
+		} else {
+				rigidbody.AddForce (-movement * speed * Time.deltaTime);
+		}
+
 	}
 }
