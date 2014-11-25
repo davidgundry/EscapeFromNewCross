c5fe08c46683eb386df0a57341bac2699551786b
Joe Cutting <Joe Cutting>
2014-11-25 12:44:53 +0000

    First Maze generation algorithm doesn't work

---
+++ Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+// adapted from code at https://gist.github.com/anonymous/828805
+
+public class MazeBuilder
+{
+
+		private const int _rowDimension = 0;
+		private const int _columnDimension = 1;
+		private const int S = 1;
+		private const int E = 2;
+		private const int HORIZONTAL = 1;
+		private const int VERTICAL = 2;
+	
+		public int MinSize { get; private set; }
+
+		public int MaxSize { get; private set; }
+
+		public Maze maze { get; private set; }
+
+		public MazeBuilder () : this (3,3)
+		{
+
+		}
+			
+		public MazeBuilder (int rows, int columns)
+		{
+				MinSize = 3;
+				MaxSize = 10;
+				maze = Initialise (rows, columns);
+		}
+	public int choose_orientation(int width, int height) {
+		if (width < height) {
+			return HORIZONTAL
+		} else {
+			if (height < width) {
+				return VERTICAL
+			} else {
+				return Random.Range(0,1) ? HORIZONTAL : VERTICAL;
+			}
+		}
+	}
+	private divide(grid, x, y, width, height, orientation) {
+		return if width < 2 || height < 2
+			
+			display_maze(grid)
+				sleep 0.02
+				
+				horizontal = orientation == HORIZONTAL
+				
+				# where will the wall be drawn from?
+				wx = x + (horizontal ? 0 : rand(width-2))
+				wy = y + (horizontal ? rand(height-2) : 0)
+				
+				# where will the passage through the wall exist?
+				px = wx + (horizontal ? rand(width) : 0)
+				py = wy + (horizontal ? 0 : rand(height))
+				
+				# what direction will the wall be drawn?
+				dx = horizontal ? 1 : 0
+				dy = horizontal ? 0 : 1
+				
+				# how long will the wall be?
+				length = horizontal ? width : height
+				
+				# what direction is perpendicular to the wall?
+				dir = horizontal ? S : E
+				
+				length.times do
+				grid[wy][wx] |= dir if wx != px || wy != py
+					wx += dx
+						wy += dy
+						end
+						
+						nx, ny = x, y
+						w, h = horizontal ? [width, wy-y+1] : [wx-x+1, height]
+						divide(grid, nx, ny, w, h, choose_orientation(w, h))
+						
+						nx, ny = horizontal ? [x, wy+1] : [wx+1, y]
+						w, h = horizontal ? [width, y+height-wy-1] : [x+width-wx-1, height]
+						divide(grid, nx, ny, w, h, choose_orientation(w, h))
+						end
+	}
+
+
+				
+			
+		public Maze Initialise (int rows, int columns)
+		{
+				if (rows < MinSize)
+						rows = MinSize;
+				
+				if (columns < MinSize)
+						columns = MinSize;
+				
+				if (rows > MaxSize)
+						rows = MaxSize;
+				
+				if (columns > MaxSize)
+						columns = MaxSize;
+				
+				var maze = new Maze ();
+				maze.create (rows, columns);
+				maze.fillValue (15);
+				
+				
+				
+				return maze;
+		}
+			
+		private Dictionary<Directions, int> DirectionX = new Dictionary<Directions, int>
+			{
+				{ Directions.N, 0 },
+				{ Directions.S, 0 },
+				{ Directions.E, 1 },
+				{ Directions.W, -1 }
+			};
+		private Dictionary<Directions, int> DirectionY = new Dictionary<Directions, int>
+			{
+				{ Directions.N, -1 },
+				{ Directions.S, 1 },
+				{ Directions.E, 0 },
+				{ Directions.W, 0 }
+			};
+		private Dictionary<Directions, Directions> Opposite = new Dictionary<Directions, Directions>
+			{
+				{ Directions.N, Directions.S },
+				{ Directions.S, Directions.N },
+				{ Directions.E, Directions.W },
+				{ Directions.W, Directions.E }
+			};
+			
+		public Maze Generate ()
+		{
+				var cells = maze.cells;
+				;
+				CarvePassagesFrom (0, 0, ref cells);
+				return maze;
+		}
+			
+		public void CarvePassagesFrom (int currentX, int currentY, ref int[,] grid)
+		{
+				var directions = new List<Directions>
+				{
+					Directions.N,
+					Directions.S,
+					Directions.E,
+					Directions.W
+				}.OrderBy (x => Guid.NewGuid ());
+				
+				foreach (var direction in directions) {
+						var nextX = currentX + DirectionX [direction];
+						var nextY = currentY + DirectionY [direction];
+					
+						if (IsOutOfBounds (nextX, nextY, grid))
+								continue;
+					
+						if (grid [nextY, nextX] != 15) // has been visited
+								continue;
+					
+						grid [currentY, currentX] &= ~((int)direction);
+						grid [nextY, nextX] &= ~((int)Opposite [direction]);
+					
+						CarvePassagesFrom (nextX, nextY, ref grid);
+				}
+		}
+			
+		private bool IsOutOfBounds (int x, int y, int[,] grid)
+		{
+				if (x < 0 || x > grid.GetLength (_rowDimension) - 1)
+						return true;
+				
+				if (y < 0 || y > grid.GetLength (_columnDimension) - 1)
+						return true;
+				
+				return false;
+		}
+			
+		/*	public void Print(int[,] grid)
+			{
+				var rows = grid.GetLength(_rowDimension);
+				var columns = grid.GetLength(_columnDimension);
+				
+				// Top line
+				Console.Write(" ");
+				for (int i = 0; i < columns; i++)
+					Console.Write(" _");
+				Console.WriteLine();
+				
+				for (int y = 0; y < rows; y++)
+				{
+					Console.Write(" |");
+					
+					for (int x = 0; x < columns; x++)
+					{
+						var directions = (Directions)grid[y, x];
+						
+						var s = directions.HasFlag(Directions.S) ? " " : "_";
+						
+						Console.Write(s);
+						
+						s = directions.HasFlag(Directions.E) ? " " : "|";
+						
+						Console.Write(s);					
+					}
+					
+					Console.WriteLine();
+				}
+			}*/
+		
+}
--- Assets/Scripts/MazeDrawScript.cs
+++ Assets/Scripts/MazeDrawScript.cs
@@ -1,14 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//Flags
-public enum Directions
-{
-	N = 1,
-	S = 2,
-	E = 4,
-	W = 8
-}
+
 
 public class MazeDrawScript : MonoBehaviour {
 
@@ -18,39 +11,46 @@ public class MazeDrawScript : MonoBehaviour {
 	public int cellHeight;
 	public int cellWidth;
 	private float halfCellWidth, halfCellHeight;
-	private int[,] cells = new int[,]{{9,3,1,5},{8,8,0,4},{8,0,6,4},{10,2,2,6}};
+	private Maze testMaze;
+	private MazeBuilder builder;
+	//private int[,] cells = new int[,]{{9,3,1,5},{8,8,0,4},{8,0,6,4},{10,2,2,6}};
 	// Use this for initialization
 	void Start () {
 		halfCellWidth = cellWidth / 2.0f;
 		halfCellHeight = cellHeight / 2.0f;
-		Vector3 wallSize = wallPrefab.renderer.bounds.size;
-		Debug.Log ("wall size =" + wallSize);
-		Vector3 Npos = new Vector3 (0, 0, -halfCellHeight+wallSize.z/2);
-		Vector3 Spos = new Vector3 (0, 0, halfCellHeight-wallSize.z/2);
-		Vector3 Wpos = new Vector3 (-halfCellWidth+wallSize.z/2, 0, 0);
-		Vector3 Epos = new Vector3 (halfCellWidth-wallSize.z/2, 0, 0);
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				int contents = cells[y,x];
-				if (hasFlag(contents,Directions.N)) {
-					makeWall(x,y,Npos,Vector3.zero);
-				}
-				if (hasFlag(contents,Directions.S)) {
-					makeWall(x,y,Spos,Vector3.zero);
-				}
-				if (hasFlag(contents,Directions.W)) {
-					makeWall(x,y,Wpos,new Vector3(0,90f,0));
-				}
-				if (hasFlag(contents,Directions.E)) {
-					makeWall(x,y,Epos,new Vector3(0,90f,0));
-				}
-			}
-		}
-	
+		builder = new MazeBuilder (4, 4);
+		testMaze= builder.maze;
+		testMaze = builder.Generate ();
+		//Maze testMaze = new Maze();
+		//testMaze.fill(4,4,new int[,]{{9,8,8,10},{3,8,0,2},{1,0,6,2},{5,4,4,6}});
+		drawMaze (testMaze);
 	}
-	bool hasFlag(int value,Directions flag) {
-				return ((value & (int)flag)>0);
+	void drawMaze(Maze drawMaze) {
+				Vector3 wallSize = wallPrefab.renderer.bounds.size;
+				Debug.Log ("wall size =" + wallSize);
+				Vector3 Npos = new Vector3 (0, 0, -halfCellHeight + wallSize.z / 2);
+				Vector3 Spos = new Vector3 (0, 0, halfCellHeight - wallSize.z / 2);
+				Vector3 Wpos = new Vector3 (-halfCellWidth + wallSize.z / 2, 0, 0);
+				Vector3 Epos = new Vector3 (halfCellWidth - wallSize.z / 2, 0, 0);
+				for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+								if (drawMaze.hasDirection(x,y,Directions.N)) {
+										makeWall (x, y, Npos, Vector3.zero);
+								}
+								if (drawMaze.hasDirection(x,y,Directions.S)) {
+										makeWall (x, y, Spos, Vector3.zero);
+								}
+								if (drawMaze.hasDirection(x,y,Directions.W)) {
+										makeWall (x, y, Wpos, new Vector3 (0, 90f, 0));
+								}
+								if (drawMaze.hasDirection(x,y,Directions.E)) {
+										makeWall (x, y, Epos, new Vector3 (0, 90f, 0));
+								}
+						}
+				}
 		}
+
+
 	void makeWall(int x, int y,Vector3 offset,Vector3 rotate) {
 		Vector3 position = new Vector3 (cellWidth * (0.5f+x-(width/2.0f)), 0, cellHeight * (0.5f+y-(height/2.0f))) + transform.position;
 				GameObject newWall = (GameObject)Instantiate (wallPrefab, position+offset, Quaternion.Euler (rotate));
@@ -58,8 +58,5 @@ public class MazeDrawScript : MonoBehaviour {
 		}
 
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
