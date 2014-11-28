c05e5a767496cf20d09cbb3835e19eb392ca8ae0
davidgundry <davidgundry@gmail.com>
2014-11-28 12:44:17 +0000

    Hiding mouse and config build settings

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -66,7 +66,7 @@ public class GameController : MonoBehaviour
 				completeGUI.SetActive (false);
 				pillsInWorld = 999;
 				Time.timeScale = 0;
-	    
+				Screen.showCursor = false;
 				startGame ();
 		}
 
